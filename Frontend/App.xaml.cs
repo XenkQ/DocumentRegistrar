9b6d42b34775f6a8bab587f1d976bdaeeb058c95
Konrad Blaszyński <blaszynski.konrad@gmail.com>
2025-06-19 12:26:31 +0200

    Add project files.

---
+++ Frontend/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using CommunityToolkit.Mvvm.DependencyInjection;
+using Frontend.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.UI.Xaml;
+
+namespace Frontend;
+
+internal sealed partial class App : Application
+{
+    private Window? _window;
+    public new static App Current => (App)Application.Current;
+
+    public App()
+    {
+        InitializeComponent();
+
+        Ioc.Default.ConfigureServices(new ServiceCollection()
+            .AddSingleton<MainWindowViewModel>()
+            .BuildServiceProvider()
+        );
+    }
+
+    protected override void OnLaunched(LaunchActivatedEventArgs args)
+    {
+        _window = new MainWindow();
+        _window.Activate();
+    }
+}
---
+++ Frontend/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.UI.Xaml;
+
+namespace Frontend;
+
+internal sealed partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
